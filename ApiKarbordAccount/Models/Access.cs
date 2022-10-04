c3be9601878ac410fab5d8245f2f06430cd56a91
karbord <karbord@SERVER>
2022-10-04 09:12:33 +0330

    test git

--- ApiKarbordAccount/Models/Access.cs
+++ ApiKarbordAccount/Models/Access.cs
@@ -18,7 +18,7 @@ namespace ApiKarbordAccount.Models
 
         public string UserName { get; set; }
 
-        public string Password { get; set; }
+        public string Password { get; set; } 
 
         public string AddressApi { get; set; }
 
